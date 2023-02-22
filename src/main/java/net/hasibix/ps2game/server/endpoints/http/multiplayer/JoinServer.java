1e45661f0c59064f1ee2b6cd22485d0b123c325f
Hasibix Hasib <hasibix3690@gmail.com>
2023-02-23 01:38:48 +0600

    Update routes

--- src/main/java/net/hasibix/ps2game/server/endpoints/http/multiplayer/JoinServer.java
+++ src/main/java/net/hasibix/ps2game/server/endpoints/http/multiplayer/JoinServer.java
@@ -1,19 +1,14 @@
 package net.hasibix.ps2game.server.endpoints.http.multiplayer;
 
 import static spark.Spark.*;
-import spark.Request;
-import spark.Response;
-import spark.Route;
 
 public class JoinServer {
     public void register() {
-        post("/servers/:sid/join", new Route() {
-            @Override public Object handle(Request request, Response response) {
-                String userID = request.queryParams("userID");
-                String serverID = request.params(":sid");
-                response.status(200);
+        post("/servers/:sid/join", (req, res) -> {
+                String userID = req.queryParams("userID");
+                String serverID = req.params(":sid");
+                res.status(200);
                 return  "Connected to server " + serverID + " as user " + userID + ".";
-            }
         });
     }
 }
