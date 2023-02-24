6ee2da0c5f07f0a0d8804588341fe6ea86ddf404
Hasibix Hasib <hasibix3690@gmail.com>
2023-02-24 15:45:15 +0600

    Routes Handler

--- src/main/java/net/hasibix/ps2game/server/handlers/RoutesHandler.java
+++ src/main/java/net/hasibix/ps2game/server/handlers/RoutesHandler.java
@@ -4,77 +4,165 @@ import static spark.Spark.*;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Set;
-import net.hasibix.ps2game.server.models.client.Route;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+
+import net.hasibix.ps2game.server.models.client.Routes;
 import net.hasibix.ps2game.server.utils.ClassLoader;
 import net.hasibix.ps2game.server.utils.Logger;
 
 public class RoutesHandler {
-    public static RoutesHandler instance;
-    private static final Logger logger = Logger.of(RoutesHandler.class);
-    public List<Route> routes;
+    public static class Rest {
+        public static Rest instance;
+        private static final Logger logger = Logger.of(Rest.class);
+        public List<Routes.Rest> routes;
 
-    private RoutesHandler() {}
+        private Rest() {}
 
-    public static RoutesHandler Intialize() {
-        if (instance == null) {
-            instance = new RoutesHandler();
+        public static Rest Intialize() {
+            if (instance == null) {
+                instance = new Rest();
+            }
+
+            return instance;
         }
 
-        return instance;
+        public void LoadRoutes(String routesPackage) {
+            try {
+                Set<Class<?>> clazzez = ClassLoader.loadFromPackage(routesPackage + ".rest");
+                
+                for (Class<?> i : clazzez) {
+                    Method register = i.getDeclaredMethod("register");
+                    register.invoke(null);
+                }
+            } catch (Exception e) {
+                logger.error("An exception occurred while trying to load rest routes.");
+                logger.trace(e);
+            }
+        }
+
+        public void AddRoute(Routes.Rest route) {
+            if(!this.routes.contains(route)) {
+                this.routes.add(route);
+            }
+        }
+
+        public void Listen(int port) {
+            for (Routes.Rest i : this.routes) {
+                switch (i.routeType) {
+                    case Get:
+                        get(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Head:
+                        head(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Post:
+                        post(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Put:
+                        put(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Delete:
+                        delete(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Connect:
+                        connect(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Options:
+                        options(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                    case Trace:
+                        trace(i.path, (req, res) -> i.run.apply(req, res));
+                        break;
+                }
+            }
+            port(port != -1 ? port : 3000);
+        }
     }
 
-    public void LoadRoutes(String routesPackage) {
-        try {
-            Set<Class<?>> clazzez = ClassLoader.loadFromPackage(routesPackage);
+    public static class WebSocket {
+
+        @org.eclipse.jetty.websocket.api.annotations.WebSocket
+        private static class Handler {
+            private Routes.WebSocket route;
             
-            for (Class<?> i : clazzez) {
-                Method register = i.getDeclaredMethod(routesPackage);
-                register.invoke(null);
+            public static Handler Intialize(Routes.WebSocket route) {
+                Handler instance = new Handler();
+                instance.route = route;
+                return instance;
             }
-        } catch (Exception e) {
-            logger.error("An exception occurred while trying to load routes.");
-            logger.trace(e);
-        }
-    }
 
-    public void AddRoute(Route route) {
-        if(!this.routes.contains(route)) {
-            this.routes.add(route);
-        }
-    }
+            @OnWebSocketConnect
+            public void onConnect(Session session) {
+                if(this.route.onConnect != null) {
+                    this.route.onConnect.run(session);
+                }
+            }
 
-    public void Listen(int port) {
-        for (Route i : this.routes) {
-            switch (i.routeType) {
-                case Rest_Get:
-                    get(i.path, i.run);
-                    break;
-                case Rest_Head:
-                    head(i.path, i.run);
-                    break;
-                case Rest_Post:
-                    post(i.path, i.run);
-                    break;
-                case Rest_Put:
-                    put(i.path, i.run);
-                    break;
-                case Rest_Delete:
-                    delete(i.path, i.run);
-                    break;
-                case Rest_Connect:
-                    connect(i.path, i.run);
-                    break;
-                case Rest_Options:
-                    options(i.path, i.run);
-                    break;
-                case Rest_Trace:
-                    trace(i.path, i.run);
-                    break;
-                case Websocket:
-                    webSocket(i.path, i.runws);
-                    break;
+            @OnWebSocketMessage
+            public void onMessage(Session session, String message) {
+                if(this.route.onMessage != null) {
+                    this.route.onMessage.run(session, message);
+                }
+            }
+
+            @OnWebSocketError
+            public void onError(Session session, Throwable e) {
+                if(this.route.onError != null) {
+                    this.route.onError.run(session, e);
+                }
+            }
+            
+            @OnWebSocketClose
+            public void onClose(Session session, int code, String reason) {
+                if(this.route.onClose != null) {
+                    this.route.onClose.run(session, code, reason);
+                }
             }
         }
-        port(port != -1 ? port : 3000);
+
+        public static WebSocket instance;
+        private static final Logger logger = Logger.of(WebSocket.class);
+        public List<Routes.WebSocket> routes;
+
+        private WebSocket() {}
+
+        public static WebSocket Intialize() {
+            if (instance == null) {
+                instance = new WebSocket();
+            }
+
+            return instance;
+        }
+
+        public void LoadRoutes(String routesPackage) {
+            try {
+                Set<Class<?>> clazzez = ClassLoader.loadFromPackage(routesPackage + ".websocket");
+                
+                for (Class<?> i : clazzez) {
+                    Method register = i.getDeclaredMethod("register");
+                    register.invoke(null);
+                }
+            } catch (Exception e) {
+                logger.error("An exception occurred while trying to load rest routes.");
+                logger.trace(e);
+            }
+        }
+
+        public void AddRoute(Routes.WebSocket route) {
+            if(!this.routes.contains(route)) {
+                this.routes.add(route);
+            }
+        }
+
+        public void Listen(int port) {
+            for (Routes.WebSocket i : this.routes) {
+                webSocket(i.path, Handler.Intialize(i));
+            }
+            port(port != -1 ? port : 3000);
+        }
     }
 }
---
+++ src/main/java/net/hasibix/ps2game/server/models/client/Routes.java
@@ -0,0 +1,68 @@
+package net.hasibix.ps2game.server.models.client;
+
+import java.util.function.BiFunction;
+import javax.annotation.Nullable;
+import org.eclipse.jetty.websocket.api.Session;
+import spark.Request;
+import spark.Response;
+
+public class Routes {
+    public static class Rest {
+        public enum Type {
+            Get,
+            Head,
+            Post,
+            Put,
+            Delete,
+            Connect,
+            Options,
+            Trace,
+        }
+    
+        public final String name;
+        public final String path;
+        public final Type routeType;
+        public final BiFunction<Request, Response, Object> run;
+    
+        public Rest(String name, String path, Type routeType,  BiFunction<Request, Response, Object> run) {
+            this.name = name;
+            this.path = path;
+            this.routeType = routeType;
+            this.run = run;
+        }
+    }
+
+    public static class WebSocket {
+        public final String name;
+        public final String path;
+
+        @FunctionalInterface
+        public interface EventFunc<A> {
+            void run(A a);
+        }
+
+        @FunctionalInterface
+        public interface BiEventFunc<A, B> {
+            void run(A a, B b);
+        }
+
+        @FunctionalInterface
+        public interface TriEventFunc<A, B, C> {
+            void run(A a, B b, C c);
+        }
+
+        @Nullable public final EventFunc<Session> onConnect;
+        @Nullable public final BiEventFunc<Session, String> onMessage;
+        @Nullable public final BiEventFunc<Session, Throwable> onError;
+        @Nullable public final TriEventFunc<Session, Integer, String> onClose;
+        
+        public WebSocket(String name, String path, @Nullable EventFunc<Session> onConnect, @Nullable BiEventFunc<Session, String> onMessage, @Nullable BiEventFunc<Session, Throwable> onError, @Nullable TriEventFunc<Session, Integer, String> onClose) {
+            this.name = name;
+            this.path = path;
+            this.onConnect = onConnect;
+            this.onMessage = onMessage;
+            this.onError = onError;
+            this.onClose = onClose;
+        }
+    }
+}
